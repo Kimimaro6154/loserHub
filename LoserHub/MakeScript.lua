44ab3dff72038877c73784d4b6a870212bde0b56
Kimimaro6154 <72808626+Kimimaro6154@users.noreply.github.com>
2020-12-29 21:20:59 +0700

    Create MakeScript.lua

---
+++ LoserHub/MakeScript.lua
@@ -0,0 +1,5 @@
+-- Simple Spy
+loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/legacy-ui/SimpleSpy.lua"))()
+
+-- Dark Dex
+loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
